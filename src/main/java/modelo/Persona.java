994303546bb85630774f8419a014d63a6103766a
AbidielLopez <abdielkacls@gmail.com>
2024-07-29 22:58:34 -0600

    primer commit

---
+++ src/main/java/modelo/Persona.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Kenneth
+ */
+public class Persona {
+        private String nombre,apellido,direccion,telefono,fechanac;
+
+    public Persona (){}
+    public Persona(String nombre, String apellido, String direccion, String telefono, String fechanac) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.fechanac = fechanac;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFechanac() {
+        return fechanac;
+    }
+
+    public void setFechanac(String fechanac) {
+        this.fechanac = fechanac;
+    }
+    
+    protected void agregar (){}
+}
---
+++ src/main/java/vista/Poo3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vista;
+
+/**
+ *
+ * @author Kenneth
+ */
+public class Poo3 {
+
+    public static void main(String[] args) {
+        frm_cliente frm = new frm_cliente();
+        frm.show();
+    }
+}
