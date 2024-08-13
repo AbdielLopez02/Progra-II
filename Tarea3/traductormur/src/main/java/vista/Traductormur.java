f794acf1c6b488b6859486c750aa045b3578d13f
AbdielLopez <72882128+AbdielLopez02@users.noreply.github.com>
2024-08-12 22:59:20 -0600

    Add files via upload

---
+++ Tarea3/traductormur/src/main/java/vista/Traductormur.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vista;
+
+/**
+ *
+ * @author Kenneth
+ */
+public class Traductormur {
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new traductor().setVisible(true);
+            }
+        });
+    }
+}
